733c353fb4af475d69072136e6d930a27c1a6a34
Ufyanich <tima.sul.2001@gmail.com>
2023-07-09 16:21:10 +0600

    Creation of Array with initial variables before

--- src/com/MyList/MyArrayList.java
+++ src/com/MyList/MyArrayList.java
@@ -1,6 +1,11 @@
 package com.MyList;
 
-public class MyArrayList<E> implements List<E> {
+public class MyArrayList{
     private Object[] arr;
     int size;
+
+    MyArrayList(){
+        this.arr = new Object[size];
+        this.size = 0;
+    }
 }
